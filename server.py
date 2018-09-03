5cb9fcba5521ef7b54ac69911372bb524f49fc67
Cesar Farrera <cfarrera@nerds.ai>
2018-09-03 08:54:09 -0500

    [0.0.1][Fix] Server no longer requires host to initialize

--- server.py
+++ server.py
@@ -4,13 +4,13 @@ import sys
 #Get host and port from command line arguments
 args = sys.argv
 args = args[1:len(args)]
-if len(args) != 2:
-    print("Usage: $python3 server.py <host> <port>");
+if len(args) != 1:
+    print("Usage: $python3 server.py <port>");
     sys.exit(1)
 
 #reads data from the input socket and sends it back
-async def handle_echo(reader, writer):
-    
+async def server_main(reader, writer):
+    clients = []
     while True:
         data = await reader.read(100)
         message = data.decode()
@@ -30,10 +30,10 @@ async def handle_echo(reader, writer):
 #start server
 try:
     loop = asyncio.get_event_loop()
-    coro = asyncio.start_server(handle_echo, args[0], args[1], loop=loop)
+    coro = asyncio.start_server(server_main, "127.0.0.1", args[0], loop=loop)
     server = loop.run_until_complete(coro)
 except Exception as ex:
-    print("No se pudo iniciar el servidor, verifica que la direccion y puerto sean correctos")
+    print("No se pudo iniciar el servidor, verifica que el puerto sea un numero entero")
     sys.exit(1)
 
 # Serve requests until Ctrl+C is pressed
