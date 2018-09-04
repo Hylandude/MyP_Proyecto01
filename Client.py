a62e2f1c09ee0d92269fa77de4a1ac6236a1b930
Cesar Farrera <cfarrera@nerds.ai>
2018-09-04 13:39:33 -0500

    [1.1.0][Milestone] Re-write client, now object oriented

--- Client.py
+++ Client.py
@@ -1,5 +1,6 @@
 import asyncio
 import sys
+from sys import stdout
 
 #Get host and port from command line arguments
 args = sys.argv
@@ -8,28 +9,53 @@ if len(args) != 2:
     print("Usage: $python3 client.py <host> <port>");
     sys.exit(1)
 
-#reads a message from command line, sends it to server and prints response
-async def tcp_echo_client(loop):
-    try:
-        reader, writer = await asyncio.open_connection(args[0], args[1], loop=loop)
-    except ConnectionRefusedError:
-        print("Error conectado al servidor, verifica la direccion y puerto")
-        sys.exit(1)
+class Client(asyncio.Protocol):
+    def __init__(self, loop, **kwargs):
+        self.is_open = False
+        self.loop = loop
 
-    close_sequence = "open"
-    while close_sequence != "close":
-        message = input("Escribe tu mensaje:\n")
-        writer.write(message.encode())
+    def connection_made(self, transport):
+        print("CONNECTION MADE")
+        self.sockname = transport.get_extra_info("sockname")
+        self.transport = transport
+        self.is_open = True
 
-        data = await reader.read(100)
-        print('Received: %r' % data.decode())
-        close_sequence = data.decode().split("_")[0]
-        print("close_sequence: "+close_sequence)
+    def connection_lost(self, exc):
+         self.is_open = False
+         self.loop.stop()
 
-    print('Close the socket')
-    writer.close()
+    def data_received(self, data):
+        print("DATA Received")
+        while not hasattr(self, "output"): #Wait until output is established
+            pass
+        if data:
+            message = data.decode()
+            print("MESSAGE: "+message+"---")
+            self.output(message+"\n")
 
-#initializes async loop for client
-loop = asyncio.get_event_loop()
-loop.run_until_complete(tcp_echo_client(loop))
+    def send(self, data):
+        if data:
+            self.transport.write(data.encode())
+
+    def initializeOutput(self, loop):
+        self.output = self.stdoutput
+        self.output("Connected to {0}:{1}\n".format(*self.sockname))
+        while True:
+            msg = input("Escribe tu mensaje\n")
+            self.send(msg)
+
+    def stdoutput(self, data):
+        stdout.write(data.strip() + '\n')
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    userClient = Client(loop)
+    coro = loop.create_connection(lambda: userClient, args[0], args[1])
+    server = loop.run_until_complete(coro)
+    asyncio.async(userClient.initializeOutput(loop))
+    loop.run_forever()
+    loop.close()
+
+
+
 loop.close()
